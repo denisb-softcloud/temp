4515404b59c09e0470ec71b89bf2ac19af3d452c
Denis Bakanov <denisb@softcloud.co.nz>
2019-11-11 10:29:18 +1300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular Material
+import { MatDialogModule } from '@angular/material';
+
+// App Dialogs
+import { NewDialogComponent } from './components/app-dialogs/new-dialog/new-dialog.component';
+import { HomeComponent } from './components/app-pages/home/home.component';
+import { HeaderComponent } from './components/app-layout/header/header.component';
+import { NewComponentComponent } from './components/app-components/new-component/new-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // App Components
+    // Dialogs
+    NewDialogComponent,
+    HomeComponent,
+    HeaderComponent,
+    NewComponentComponent
   ],
   imports: [
+    // Basic Modules
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    // Material Modules
+    MatDialogModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    NewDialogComponent,
+  ]
 })
 export class AppModule { }
